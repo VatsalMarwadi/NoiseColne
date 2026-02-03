6ed27624d87d02a1be2351679ab02c1198b578da
VatsalMarwadi <vatsalmarwadi2@gmail.com>
2026-02-03 14:26:50 +0530

    Full Project

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Banner.module.css";
 
-import slider1 from "../../../assets/slider1.png";
-import slider2 from "../../../assets/slider2.png";
-import slider3 from "../../../assets/slider3.png";
-import slider4 from "../../../assets/slider4.png";
+import slider1 from "../../assets/slider1.png";
+import slider2 from "../../assets/slider2.png";
+import slider3 from "../../assets/slider3.png";
+import slider4 from "../../assets/slider4.png";
 
 const SLIDES = [
   { image: slider1, link: "#smart-watches" },
